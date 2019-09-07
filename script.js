9fddbcb6233e8f97d16054a6303d2398c1ed61ad
suely <suelyribeirocm@gmail.com>
2019-09-06 21:50:25 -0300

    Aula de bolinhas

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+$(document).ready(funcion)(){
+                 
+    $('.red').click(funcion(){
+                    
+    (this).fadeOut();                
+    
+});            
+            
+ $(.green').click(funcion(){
+                   
+    $('.blue').hide ();             
+                                  
+                 
+});
 
+$('.yellow').click(funcion(){
+                 
+    $('.blue').fadeIn();
+    $('.green').hide();
+});
+
+$('.blue').click(funcion(){
+    $('.red, .green; .yellow').toggle();
+
+});
+                 
